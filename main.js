604c8138128b349ed80fff52c33de449bbd1d1b9
“delanoklee” <delanoklee@users.noreply.github.com>
2026-01-28 22:54:59 -0700

    Add website and build workflow

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, screen } = require('electron')
+
+try {
+  require('electron-reloader')(module)
+} catch {}
+
+function createWindow() {
+  const display = screen.getPrimaryDisplay()
+  const { width: screenWidth } = display.workAreaSize
+
+  // Window is 50% of screen width, centered at top
+  const windowWidth = Math.round(screenWidth * 0.5)
+  const windowHeight = 60
+
+  const win = new BrowserWindow({
+    width: windowWidth,
+    height: windowHeight,
+    x: Math.round((screenWidth - windowWidth) / 2),
+    y: 0,
+    frame: false,
+    resizable: true,
+    alwaysOnTop: true
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
